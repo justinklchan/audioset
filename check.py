106c1c744de15782983d994dd4f1b90a9b0be7a6
Justin Chan <chan.justin.hk@gmail.com>
2022-08-06 23:36:07 -0700

    a

--- check.py
+++ check.py
@@ -1,7 +1,12 @@
 import youtube_dl
 from tqdm import tqdm
+import signal
+import sys
 
-def download(vid):
+def handler(signum, frame):
+	sys.exit(0)
+
+def download(id,vid):
 	ydl_opts = {
 	    'format': 'bestaudio/best',
 	    # 'postprocessors': [{
@@ -11,23 +16,32 @@ def download(vid):
 	    # }],
     	# 'audioformat':'mp3',
 	    'extractaudio':True,
-	    'outtmpl': u'%(id)s.%(ext)s',
+	    'outtmpl': id+u'%(id)s.%(ext)s',
 	}
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 	    ydl.download(['http://www.youtube.com/watch?v='+vid])
 
-id='/m/015p6'
+signal.signal(signal.SIGINT, handler)
 
-for file in ['eval_segments','balanced_train_segments','unbalanced_train_segments']:
-	lines=open(file+'.csv').read().split('\n')
-	vids=[]
-	for line in lines:
-		if id in line:
-			elts=line.split(',')
-			if len(elts)-3==1:
-				vids.append(elts[0])
-	for vid in tqdm(vids):
-		try :
-			download(vid)
-		except:
-			print ('err ',vid)
+
+for id in ['/m/015p6']:
+	if not os.path.exists(id):
+		os.mkdir('_'+id)
+	for file in ['eval_segments','balanced_train_segments','unbalanced_train_segments']:
+		lines=open(file+'.csv').read().split('\n')
+		vids=[]
+		for line in lines:
+			if id in line:
+				elts=line.split(',')
+				if len(elts)-3==1:
+					vids.append(elts[0])
+
+		counter=0
+		for vid in tqdm(vids):
+			try :
+				download(id,vid)
+				counter+=1
+				if counter == 350:
+					break
+			except:
+				print ('err ',vid)
