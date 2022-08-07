9a84a379a3054c1976a851e6f6f9c8fb70778b16
Justin Chan <chan.justin.hk@gmail.com>
2022-08-07 00:11:21 -0700

    a

--- check.py
+++ check.py
@@ -67,7 +67,7 @@ for id in ids:
 	
 	files=os.listdir(file_id)
 	n_files=len(files)
-	target_n=350-n_files+1
+	target_n=100-n_files+1
 
 	print ('TARGET ',target_n)
 	for file in ['eval_segments','balanced_train_segments','unbalanced_train_segments']:
