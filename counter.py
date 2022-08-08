cd41f2274bc72e7fb0e9ca43b745262a552f0708
justinklchan <chan.justin.hk@gmail.com>
2022-08-07 21:49:13 -0700

    A

---
+++ counter.py
@@ -0,0 +1,25 @@
+import os
+ids=['/m/015p6',
+'/m/02rlv9',
+'/m/07rkbfh',
+'/m/09x0r',
+'/m/05kq4',
+'/m/0cmf2',
+'/m/03m9d0z',
+'/m/0838f',
+'/m/03wwcy',
+'/m/0c3f7m',
+'/t/dd00002',
+'/m/0btp2',
+'/m/03p19w',
+'/m/0d31p',
+'/m/03kmc9',
+'/m/07qwf61',
+'/m/07q7njn',
+'/m/04rlf',
+'/m/03qtwd',
+'/m/04229']
+
+for i in ids:
+	fs=os.listdir(i)
+	print (i,len(fs))
