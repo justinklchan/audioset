3496f09e005f4e9dc9ce8ed89aee4b7708734054
justinklchan <chan.justin.hk@gmail.com>
2022-08-07 21:58:23 -0700

    A

--- counter.py
+++ counter.py
@@ -1,28 +1,35 @@
 import os
-# ids=['/m/015p6',
-# '/m/02rlv9',
-# '/m/07rkbfh',
-# '/m/09x0r',
-# '/m/05kq4',
-# '/m/0cmf2',
-# '/m/03m9d0z',
-# '/m/0838f',
-# '/m/03wwcy',
-# '/m/0c3f7m',
-# '/t/dd00002',
-# '/m/0btp2',
-# '/m/03p19w',
-# '/m/0d31p',
-# '/m/03kmc9',
-# '/m/07qwf61',
-# '/m/07q7njn',
-# '/m/04rlf',
-# '/m/03qtwd',
-# '/m/04229']
+ids=['/m/015p6',
+'/m/02rlv9',
+'/m/07rkbfh',
+'/m/09x0r',
+'/m/05kq4',
+'/m/0cmf2',
+'/m/03m9d0z',
+'/m/0838f',
+'/m/03wwcy',
+'/m/0c3f7m',
+'/t/dd00002',
+'/m/0btp2',
+'/m/03p19w',
+'/m/0d31p',
+'/m/03kmc9',
+'/m/07qwf61',
+'/m/07q7njn',
+'/m/04rlf',
+'/m/03qtwd',
+'/m/04229']
 
+lines=open('class_labels_indices.csv').read().split('\n')
 
 for i in ids:
+
+	name=""
+	for j in lines:
+		if i in j:
+			name=j.split(',')[-1]
+			break
+
 	ni=i.replace('/','_')
 	fs=os.listdir(ni)
-	if len(fs)<301:
-		print (ni,len(fs))
+	print (ni,name,len(fs))
