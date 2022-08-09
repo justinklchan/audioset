b5be08343398f9e1fb2183e853697a81d43ea974
justinklchan <chan.justin.hk@gmail.com>
2022-08-08 17:07:57 -0700

    A

--- urban.py
+++ urban.py
@@ -4,8 +4,8 @@ className='car_horn'
 lines=open('/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/metadata/UrbanSound8K.csv').read().split('\n')[1:]
 for line in lines:
 	elts=line.split(',')
-	print (line)
 	if elts[-1]==className:
+		print (line)
 		fold=elts[-3]
 		fid=elts[0]
 		oname='/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/metadata/audio/fold'+str(fold)+'/'+str(fid)
