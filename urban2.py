0ec4d5e1b8dac22a475b26a6927deae8f5730ba8
justinklchan <chan.justin.hk@gmail.com>
2022-08-08 17:30:20 -0700

    A

---
+++ urban2.py
@@ -0,0 +1,17 @@
+import shutil
+import sys
+import os
+from tqdm import tqdm
+
+className=sys.argv[1]
+
+if not os.path.exists('/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/audio/'+className+'_subset'):
+	os.mkdir('/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/audio/'+className+'_subset')
+
+fs=sorted(os.listdir('/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/audio/'+className))
+
+for i in tqdm(range(350)):
+	oname='/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/audio/'+className+'/'+fs[i]
+	nname='/gscratch/cse/jucha/audioset/UrbanSounds8K/UrbanSound8K/audio/'+className+'_subset/'+fs[i]
+	shutil.copy(oname,nname)
+	print (nname)
