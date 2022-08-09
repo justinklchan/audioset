348dbc7572fccfd38360f1305f06486b79da066a
justinklchan <chan.justin.hk@gmail.com>
2022-08-08 18:26:24 -0700

    a

--- check.py
+++ check.py
@@ -1,26 +1,32 @@
+# ids=['/m/015p6',
+# '/m/02rlv9',
+# '/m/07rkbfh',
+# '/m/09x0r',
+# '/m/05kq4',
+# '/m/0cmf2',
+# '/m/03m9d0z',
+# '/m/0838f',
+# '/m/03wwcy',
+# '/m/0c3f7m',
+# '/t/dd00002',
+# '/m/0btp2',
+# '/m/03p19w',
+# '/m/0d31p',
+# '/m/03kmc9',
+# '/m/07qwf61',
+# '/m/07q7njn',
+# '/m/04rlf',
+# '/m/03qtwd',
+# '/m/04229',
+# '/m/07qfr4h',
+# '/m/0912c9',
+# '/m/04229']
 ids=['/m/015p6',
-'/m/02rlv9',
-'/m/07rkbfh',
-'/m/09x0r',
-'/m/05kq4',
-'/m/0cmf2',
-'/m/03m9d0z',
-'/m/0838f',
-'/m/03wwcy',
-'/m/0c3f7m',
 '/t/dd00002',
-'/m/0btp2',
-'/m/03p19w',
 '/m/0d31p',
-'/m/03kmc9',
-'/m/07qwf61',
-'/m/07q7njn',
-'/m/04rlf',
-'/m/03qtwd',
-'/m/04229',
-'/m/07qfr4h',
-'/m/0912c9',
-'/m/04229']
+'/m/0cmf2',
+'/m/0838f',
+'/m/05kq4']
 
 import youtube_dl
 from tqdm import tqdm
@@ -73,7 +79,7 @@ for id in ids:
 	
 	files=os.listdir(file_id)
 	n_files=len(files)
-	target_n=400-n_files+1
+	target_n=500-n_files+1
 
 	print ('TARGET ',target_n)
 	for file in ['eval_segments','balanced_train_segments','unbalanced_train_segments']:
