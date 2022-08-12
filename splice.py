8ce98250b05f2e467f0dcfa9f0a1d7bed0a47fa7
justinklchan <chan.justin.hk@gmail.com>
2022-08-11 18:31:19 -0700

    a

--- splice.py
+++ splice.py
@@ -1,26 +1,4 @@
-ids=['/m/015p6',
-'/m/02rlv9',
-'/m/07rkbfh',
-'/m/09x0r',
-'/m/05kq4',
-'/m/0cmf2',
-'/m/03m9d0z',
-'/m/0838f',
-'/m/03wwcy',
-'/m/0c3f7m',
-'/t/dd00002',
-'/m/0btp2',
-'/m/03p19w',
-'/m/0d31p',
-'/m/03kmc9',
-'/m/07qwf61',
-'/m/07q7njn',
-'/m/04rlf',
-'/m/03qtwd',
-'/m/04229',
-'/m/07qfr4h',
-'/m/0912c9',
-'/m/04229']
+ids=['/m/07pbtc8']
 
 import os
 import sys
